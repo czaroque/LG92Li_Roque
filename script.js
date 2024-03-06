9f24383ead9f34a8242edae6fe545d37e65a6f7d
si zar <143508729+czaroque@users.noreply.github.com>
2024-03-06 23:51:13 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function DirectToSecondPage(){
+    alert("Redirecting to the second page ^_^ ...");
+    window.open("nextpage.html");
+}
+
+function ReplaceImg(){
+    hbody.src='moon.jpg'
+}
+
+function RevertImg(){
+    hbody.src='sun.jpg'
+}
